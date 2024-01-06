6bd0d08dec90587fde754487d6bd0c1d97f15c36
Ali Ghaffari <aligh71@yahoo.com>
2024-01-06 21:07:47 +0330

    create Contacts component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header.jsx";
+import Contacts from "./components/Contacts.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <Contacts />
     </>
   );
 }
---
+++ src/components/Contacts.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+function Contacts() {
+  const [contact, setContact] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+  const changeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setContact((contact) => ({ ...contact, [name]: value }));
+  };
+  const addHandler = () => {
+    console.log(contact);
+  };
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={contact.name}
+          onChange={changeHandler}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+          value={contact.lastName}
+          onChange={changeHandler}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={contact.email}
+          onChange={changeHandler}
+        />
+        <input
+          type="number"
+          placeholder="Phone"
+          name="phone"
+          value={contact.phone}
+          onChange={changeHandler}
+        />
+        <button onClick={addHandler}>Add Contact</button>
+      </div>
+    </div>
+  );
+}
+
+export default Contacts;
