c39a642842ef27a95acc99abe0af75d81f5313b8
Ali Ghaffari <aligh71@yahoo.com>
2024-01-08 18:02:00 +0330

    rendering contacts

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,13 +1,27 @@
 function ContactsList({ contacts }) {
-  console.log(contacts);
   return (
     <div>
       <h3>Contacts List</h3>
-      <ul>
-        {contacts.map((contact) => (
-          <li key={contact.id}>{contact.name}</li>
-        ))}
-      </ul>
+      {contacts.length ? (
+        <ul>
+          {contacts.map((contact) => (
+            <li key={contact.id}>
+              <p>
+                {contact.name} {contact.lastName}
+              </p>
+              <p>
+                <span>🗨</span> {contact.email}
+              </p>
+              <p>
+                <span>📞</span> {contact.phone}
+              </p>
+              <button>delete</button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No Contacts Yet!</p>
+      )}
     </div>
   );
 }
